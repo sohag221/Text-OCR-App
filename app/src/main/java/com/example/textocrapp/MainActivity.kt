6389abbbe12c5ad2901152b6a3c951cf955ff80d
sohag221 <mdratulsohag1234@gmail.com>
2024-10-27 12:23:57 +0600

    Ui design done.

--- app/src/main/java/com/example/textocrapp/MainActivity.kt
+++ app/src/main/java/com/example/textocrapp/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.textocrapp
 
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var cameraImage:ImageView
+    private lateinit var captureImageBtn:Button
+    private lateinit var resultText:TextView
+    private lateinit var copyTextBtn:Button
+
+    private  var currentPhotoPath:String? = null
+
+
+    private lateinit var requestPermissionLauncher : ActivityResultLauncher<String>
+    private lateinit var takePictureLauncher:ActivityResultLauncher<Uri>
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +43,9 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
-    }
+
+
+
 }
